16d133f166498e1f9575953667ff32914d9a749b
astro <soalejandro@protonmail.com>
2025-10-02 15:10:00 -0500

    anotacion repository jsjs

--- src/main/java/com/example/basicloggin/repository/UserRepository.java
+++ src/main/java/com/example/basicloggin/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.basicloggin.repository;
 
 import com.example.basicloggin.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel,Long> {
 }
